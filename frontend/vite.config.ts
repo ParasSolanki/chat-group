d8ed1c1e5bad30ce4ae9a73b5beda9942a91b08e
parasSolanki <parassolanki151@gmail.com>
2022-06-13 21:17:30 +0530

    removed extra stuff and basic tailwind and ts setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import * as path from 'path'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
 })
